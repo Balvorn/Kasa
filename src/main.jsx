2862b6581c6c333474fc3f1ae8786efc346d5d96
Balvorn <leo-marcou@hotmail.fr>
2024-08-20 14:41:31 +0200

    annonce page responsive

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import "./index.css";
+import "./index.scss";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
 import About from "./routes/about";
